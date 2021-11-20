92ecf20308ee048909ff368b6a58fe4d2ab05cb9
Leo Kosta <kostaleonard@gmail.com>
2021-11-19 22:21:15 -0500

    Updated docstrings
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- mlops/model/versioned_model.py
+++ mlops/model/versioned_model.py
@@ -1,6 +1,7 @@
 """Contains the VersionedModel class."""
 # pylint: disable=no-name-in-module
 
+from typing import Callable, Any
 from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import History
 from mlops import ENDPOINT_LOCAL, ENDPOINT_S3
@@ -8,7 +9,7 @@ from mlops.dataset.versioned_dataset import VersionedDataset
 
 
 class VersionedModel:
-    """TODO"""
+    """Represents a versioned model."""
     versioned_dataset: VersionedDataset
     model: Model
     history: History
@@ -28,6 +29,22 @@ class VersionedModel:
 
     @staticmethod
     def get_best(paths: list[str],
+                 endpoint: str = ENDPOINT_LOCAL,
                  metric: str = 'val_loss',
-                 function: str = 'min') -> 'VersionedModel':
-        """TODO"""
+                 comparator: Callable[[Any, Any], Any] = min) -> \
+            'VersionedModel':
+        """Returns an instance of the best VersionedModel from the given paths.
+
+        :param paths: A list of the VersionedModel paths to check.
+        :param endpoint: The system from which the model files should be
+            loaded. 'local' loads the files from the local filesystem; 's3'
+            loads the files from S3, in which case path should be an S3 URL.
+        :param metric: The name of the metric in the model's training history
+            to use as the basis of comparison for determining the best model.
+            This metric is often the validation loss.
+        :param comparator: The function to use to compare model metric values.
+            It takes two model metric values and returns the more desirable of
+            the two. For example, min will return the model with the lowest
+            metric value, say, validation loss.
+        """
+        # TODO
