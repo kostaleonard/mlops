84742ce7a54dc1eb7c4620fea69ba6622b528783
Leo Kosta <kostaleonard@gmail.com>
2021-11-23 11:33:57 -0500

    Added sample dataset
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- tests/dataset/test_data_processor.py
+++ tests/dataset/test_data_processor.py
@@ -1,7 +1,10 @@
 """Tests data_processor.py."""
 
+import pytest
+from mlops.dataset import data_processor as dp
 
-def test_placeholder() -> None:
-    """Placeholder that indicates the module has not been tested."""
-    # TODO add tests for module
-    assert False
+
+def test_data_processor_is_abstract() -> None:
+    """Tests that the DataProcessor object is abstract."""
+    with pytest.raises(TypeError):
+        _ = dp.DataProcessor()
