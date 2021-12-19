cc682e0cdc084235fa7a6577ebff5a266965772c
Leo Kosta <kostaleonard@gmail.com>
2021-12-19 10:24:29 -0800

    Removed feature and added issue for later consideration
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- mlops/model/versioned_model.py
+++ mlops/model/versioned_model.py
@@ -16,25 +16,4 @@ class VersionedModel:
         """
         # TODO get model, dataset path
 
-    @staticmethod
-    def get_best(paths: list[str],
-                 metric: str = 'val_loss',
-                 comparator: Callable[[Any, Any], Any] = min) -> \
-            'VersionedModel':
-        """Returns an instance of the best VersionedModel from the given paths.
-
-        :param paths: A list of the VersionedModel paths to check. Each path may
-            be on the local filesystem or remote, independent of the other
-            paths. An S3 path should be a URL of the form
-            "s3://bucket-name/path/to/dir".
-        :param metric: The name of the metric in the model's training history
-            to use as the basis of comparison for determining the best model.
-            This metric is often the validation loss.
-        :param comparator: The function to use to compare model metric values.
-            It takes two model metric values and returns the more desirable of
-            the two. For example, min will return the model with the lowest
-            metric value, say, validation loss.
-        """
-        # TODO
-
     # TODO mirror VersionedDataset interface, inc. __eq__ and __hash__
