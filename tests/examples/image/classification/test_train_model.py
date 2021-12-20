a8cddcd673b0ff1f60d3c8b84026a0047d7917c1
Leo Kosta <kostaleonard@gmail.com>
2021-12-20 07:11:56 -0800

    Added test function prototypes
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- tests/examples/image/classification/test_train_model.py
+++ tests/examples/image/classification/test_train_model.py
@@ -1,6 +1,28 @@
 """Tests train_model.py."""
 
+TEST_PUBLICATION_PATH_LOCAL = '/tmp/test_train_model/models'
 
-def test_placeholder() -> None:
-    """Placeholder test indicating that the module has not been tested."""
+
+def test_get_baseline_model_correct_shapes() -> None:
+    """Tests that the baseline model has the correct input and output shapes."""
+    # TODO
+    assert False
+
+
+def test_train_model_creates_checkpoints() -> None:
+    """Tests that train_model creates model checkpoints."""
+    # TODO
+    assert False
+
+
+def test_train_model_returns_correct_training_config() -> None:
+    """Tests that train_model returns a TrainingConfig object with the correct
+    information."""
+    # TODO
+    assert False
+
+
+def test_publish_model_creates_files() -> None:
+    """Tests that publish_model creates the published model files."""
+    # TODO
     assert False
