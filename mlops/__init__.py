b165d9b5a7f8ab52f5b98e66fd687b1af12d2dc9
Leo Kosta <kostaleonard@gmail.com>
2021-12-23 07:54:37 -0800

    Removed setup.py
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- mlops/__init__.py
+++ mlops/__init__.py
@@ -1,15 +1,12 @@
 """mlops is a package for conducting MLOps, including versioning of datasets and
 models."""
 
-__version__ = '0.0.13'
-
-import mlops.dataset
-import mlops.errors
-import mlops.model
+__version__ = '0.0.15'
 
 __all__ = [
     '__version__',
     'dataset',
     'errors',
-    'model'
+    'model',
+    'hashing'
 ]
