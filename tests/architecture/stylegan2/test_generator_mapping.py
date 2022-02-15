545ac8ccf53942d220b27f1858d1dc89c48fbc64
Leo Kosta <kostaleonard@gmail.com>
2022-02-15 09:07:34 -0500

    Added trainable variables test
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- tests/architecture/stylegan2/test_generator_mapping.py
+++ tests/architecture/stylegan2/test_generator_mapping.py
@@ -4,6 +4,7 @@ import pytest
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Input, Dense, ReLU, LeakyReLU
+from tensorflow.keras.backend import count_params
 from mlops.architecture.stylegan2.generator_mapping import GeneratorMapping
 
 
@@ -202,7 +203,41 @@ def test_conditioning_weights_correct_shape() -> None:
                                                       z_latent_size)
 
 
-# TODO test conditioning weights correct shape
+def test_correct_num_trainable_variables() -> None:
+    """Tests that the layer has the correct number of trainable variables
+    (identical to trainable weights) when no conditioning labels are used."""
+    z_latent_size = 4
+    mapping_fmaps = 256
+    mapping_layers = 8
+    d_latent_size = 128
+    model = Sequential([
+        Input(shape=(z_latent_size,)),
+        GeneratorMapping(z_latent_size=z_latent_size,
+                         mapping_fmaps=mapping_fmaps,
+                         mapping_layers=mapping_layers,
+                         d_latent_size=d_latent_size)])
+    first_dense_weights = (z_latent_size + 1) * mapping_fmaps
+    middle_dense_weights = (mapping_fmaps + 1) * mapping_fmaps
+    last_dense_weights = (mapping_fmaps + 1) * d_latent_size
+    expected_weights = first_dense_weights + \
+        (mapping_layers - 2) * middle_dense_weights + \
+        last_dense_weights
+    trainable_variable_sum = sum(
+        count_params(variables) for variables in model.trainable_variables)
+    assert expected_weights == trainable_variable_sum
+
+
+def test_correct_num_trainable_variables_with_conditioning() -> None:
+    """Tests that the layer has the correct number of trainable variables
+    (identical to trainable weights) when no conditioning labels are used."""
+    z_latent_size = 4
+    label_size = 2
+    model = Sequential([
+        Input(shape=(z_latent_size + label_size,)),
+        GeneratorMapping(z_latent_size=z_latent_size,
+                         label_size=label_size)])
+    model.summary()
+
 
 # TODO test trainable variables
 
